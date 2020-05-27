6878b3b6413444897fc2a062459eedc97b28fb97
Sławomir Kusiński <skwinged@gmail.com>
2020-05-27 13:09:09 +0200

    Feature: Parser str: Implementation

--- src/parsers/str.ts
+++ src/parsers/str.ts
@@ -1,11 +1,25 @@
 import { Parser } from './../parser'
-import { ValidState, IntermediateState } from '../state'
+import { ValidState, IntermediateState, ResultState, ErrorState } from '../state'
 
 export const str = (pattern: string): Parser<string> =>
   Parser.of((state: ValidState<any>): IntermediateState<string> => {
-    return {
-      __type__: 'ErrorState',
-      msg: 'Not Implemented',
-      index: 0
+    const {
+      text,
+      index
+    } = state
+
+    const shift = pattern.length
+
+    if (text.startsWith(pattern)) {
+      return ResultState(
+        pattern,
+        text.slice(shift),
+        index + shift
+      )
     }
+
+    return ErrorState(
+      `str: was looking for "${pattern}", found "${text.slice(0, shift)}"`,
+      index
+    )
   })
--- src/state.ts
+++ src/state.ts
@@ -17,12 +17,25 @@ export const InitialState = (text: string): InitialState => ({
 interface ResultState<T> {
   readonly __type__: 'ResultState'
   readonly result: T
-  readonly text: string
   readonly index: number
+  readonly text: string
 }
+
+export const ResultState = <T>(result: T, text: string, index: number): ResultState<T> => ({
+  __type__: 'ResultState',
+  result,
+  index,
+  text
+})
 
 interface ErrorState {
   readonly __type__: 'ErrorState'
   readonly msg: string
   readonly index: number
 }
+
+export const ErrorState = (msg: string, index: number): ErrorState => ({
+  __type__: 'ErrorState',
+  msg,
+  index
+})
