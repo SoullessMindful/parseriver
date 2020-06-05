d79f6358dc1cef067e59f74dbf98a4be410def81
Sławomir Kusiński <skwinged@gmail.com>
2020-06-05 11:47:27 +0200

    Feature: Parser letter: Implementation

--- src/parsers/letter.ts
+++ src/parsers/letter.ts
@@ -1,6 +1,18 @@
 import { Parser } from './../parser'
 import { ValidState, IntermediateState, ResultState, ErrorState } from '../state'
 
-export const letter = Parser.of((state: ValidState<any>): IntermediateState<string> =>
-  ErrorState(state, 'letter: Not implemented')
-)
+const A_LETTER_IN_FRONT = /^[a-zA-Z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u024F\u1E00-\u1EFF]/
+// Latin extended characters without C, D and E blocks
+
+export const letter = Parser.of((state: ValidState<any>): IntermediateState<string> => {
+  const match = state.text.match(A_LETTER_IN_FRONT)
+
+  if (match != null) {
+    return ResultState.update(
+      state,
+      state.text.charAt(0),
+      1
+    )
+  }
+  return ErrorState(state, `Parser letter: was trying to parse a letter but found "${state.text.charAt(0)}" instead`)
+})
