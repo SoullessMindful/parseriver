cc082bb1e51dfc2aa879d7772bfa8e67a1b9f96b
Sławomir Kusiński <skwinged@gmail.com>
2020-06-25 14:52:01 +0200

    Feature: Parser letter: Refactorization into Parser regexp

--- src/parsers/letter.ts
+++ src/parsers/letter.ts
@@ -1,18 +1,28 @@
 import { Parser } from './../parser'
-import { ValidState, IntermediateState, ResultState, ErrorState } from '../state'
+import {
+  ValidState,
+  IntermediateState,
+  ResultState,
+  ErrorState
+} from '../state'
+
+const regexp = (name: string, r: RegExp): Parser<string> =>
+  Parser.from(
+    (state: ValidState<any>): IntermediateState<string> => {
+      const match = state.text.match(r)
+      if (match != null) {
+        return ResultState.update(state, state.text.charAt(0), 1)
+      }
+      return ErrorState(
+        state,
+        `Parser ${name}: was trying to parse ${name} but found "${state.text.charAt(
+          0
+        )}" instead`
+      )
+    }
+  )
 
 const A_LETTER_IN_FRONT = /^[a-zA-Z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u024F\u1E00-\u1EFF]/
 // Latin extended characters without C, D and E blocks
 
-export const letter = Parser.from((state: ValidState<any>): IntermediateState<string> => {
-  const match = state.text.match(A_LETTER_IN_FRONT)
-
-  if (match != null) {
-    return ResultState.update(
-      state,
-      state.text.charAt(0),
-      1
-    )
-  }
-  return ErrorState(state, `Parser letter: was trying to parse a letter but found "${state.text.charAt(0)}" instead`)
-})
+export const letter = regexp('letter', A_LETTER_IN_FRONT)
