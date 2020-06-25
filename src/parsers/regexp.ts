4e6dab5b47a4a4dd7e74f23bb93d77c92cfeaa3e
Sławomir Kusiński <skwinged@gmail.com>
2020-06-25 15:55:12 +0200

    Feature: Parser regexp: docs added

--- src/parsers/regexp.ts
+++ src/parsers/regexp.ts
@@ -27,27 +27,56 @@ const regexp = (name: string, r: RegExp): Parser<string> =>
     }
   )
 
+// Matches latin extended letter without C, D and E blocks
 const A_LETTER_IN_FRONT = /^[a-zA-Z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u024F\u1E00-\u1EFF]/
-// Latin extended characters without C, D and E blocks
 
+/**
+ * parses a latin extended letter
+ */
 export const letter = regexp('letter', A_LETTER_IN_FRONT)
 
+// Matches a latin letter
 const A_LETTER_LATIN_IN_FRONT = /^[a-zA-Z]/
 
+/**
+ * parses a latin letter
+ */
 export const letterLatin = regexp('letterLatin', A_LETTER_LATIN_IN_FRONT)
 
+// Matches an uppercase latin letter
 const A_LETTER_LATIN_UPPER_IN_FRONT = /^[A-Z]/
 
-export const letterLatinUpper = regexp('letterLatinUpper', A_LETTER_LATIN_UPPER_IN_FRONT)
+/**
+ * parses an uppercase latin letter
+ */
+export const letterLatinUpper = regexp(
+  'letterLatinUpper',
+  A_LETTER_LATIN_UPPER_IN_FRONT
+)
 
+// Matches a lowercase latin letter
 const A_LETTER_LATIN_LOWER_IN_FRONT = /^[a-z]/
 
-export const letterLatinLower = regexp('letterLatinLower', A_LETTER_LATIN_LOWER_IN_FRONT)
+/**
+ * parses a lowercase latin letter
+ */
+export const letterLatinLower = regexp(
+  'letterLatinLower',
+  A_LETTER_LATIN_LOWER_IN_FRONT
+)
 
+// Matches a digit
 const A_DIGIT_IN_FRONT = /^[0-9]/
 
+/**
+ * parses a digit
+ */
 export const digit = regexp('digit', A_DIGIT_IN_FRONT)
 
+// Matches a nonzero digit
 const A_DIGIT_NONZERO_IN_FRONT = /^[1-9]/
 
+/**
+ * parses a digit, except for 0
+ */
 export const digitNonzero = regexp('digitNonzero', A_DIGIT_NONZERO_IN_FRONT)
