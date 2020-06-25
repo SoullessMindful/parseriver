1811d66843e32dcab432bbf126b9a3b793a92f6d
Sławomir Kusiński <skwinged@gmail.com>
2020-06-25 15:22:10 +0200

    Feature: Parser regexp: Refactor: letter.ts changed to regexp.ts and jsdocs added

--- src/parsers/regexp.ts
+++ src/parsers/regexp.ts
@@ -6,12 +6,17 @@ import {
   ErrorState
 } from '../state'
 
+/**
+ * Parser that checks if a given regular expression is matched by the parsed text
+ * @param name A name of resulting parser for the purpose of creating error messages
+ * @param r A regular expression to be tested
+ */
 const regexp = (name: string, r: RegExp): Parser<string> =>
   Parser.from(
     (state: ValidState<any>): IntermediateState<string> => {
       const match = state.text.match(r)
       if (match != null) {
-        return ResultState.update(state, state.text.charAt(0), 1)
+        return ResultState.update(state, match[0], match[0].length)
       }
       return ErrorState(
         state,
