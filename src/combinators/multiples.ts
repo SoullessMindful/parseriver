fdfb01084dfab9b2a7ac452b55bb745f7f774c82
Sławomir Kusiński <skwinged@gmail.com>
2020-06-29 18:56:47 +0200

    Feature: Combinator multiples: JSDocs added

--- src/combinators/multiples.ts
+++ src/combinators/multiples.ts
@@ -20,7 +20,7 @@ export const optional = <T>(parser: Parser<T>): Parser<T> =>
 
 /**
  * Takes two numbers and parser and returns a parser, that tries too match the given parser the amount of times between the two given numbers
- * It fails only when when there too little matches
+ * It fails only when when there is too little matches
  * In case there is too many matches it just stops parsing
  * @param lowerBound A minimal number of matches
  * @param upperBound A maximal number of matches
@@ -52,14 +52,37 @@ export const between = <T>(lowerBound: number, upperBound: number) => (parser: P
   })
 }
 
+/**
+ * Takes a number and parser and returns a parser, that tries to match the given parser at least the given number times
+ * It fails only when when there is too little matches
+ * @param lowerBound A minimal number of matches
+ * @param parser A parser to be repeatedly matched
+ */
 export const atLeast = <T>(lowerBound: number): ((parser: Parser<T>) => Parser<T[]>) =>
   between(lowerBound, Infinity)
 
+/**
+ * Takes a number and parser and returns a parser, that tries to match the given parser repeatedly at most the given number times
+ * It never fails, in case there is too many matches it just stops parsing
+ * @param upperBound A maximal number of matches
+ * @param parser A parser to be repeatedly matched
+ */
 export const atMost = <T>(upperBound: number): ((parser: Parser<T>) => Parser<T[]>) =>
   between(0, upperBound)
 
+/**
+ * Takes a parser and returns a parser, that tries to match the given parser until it fails
+ * It never fails, if there are no matches the result is an empty array
+ * @param parser A parser to be repeatedly matched
+ */
 export const many: <T>(parser: Parser<T>) => Parser<T[]> =
   atLeast(0)
 
+/**
+ * Takes a number and a parser and returns a parser, that tries to match the given parser exactly the given number times
+ * It fails only when when there is too little matches
+ * In case there is too many matches it just stops parsing
+ * @param parser A parser to be repeatedly matched
+ */
 export const exactly = <T>(times: number): (parser: Parser<T>) => Parser<T[]> =>
   between(times, times)
