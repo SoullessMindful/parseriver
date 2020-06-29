654551d244b036ca984635696aa9d499684af109
Sławomir Kusiński <skwinged@gmail.com>
2020-06-29 16:57:51 +0200

    Feature: Combinator multiples: between combinator function added

--- src/combinators/multiples.ts
+++ src/combinators/multiples.ts
@@ -1,3 +1,4 @@
+import { ValidState, ParserState, ErrorState } from './../state';
 import { Parser } from './../parser'
 import { ResultState } from '../state'
 
@@ -17,3 +18,37 @@ export const optional = <T>(parser: Parser<T>): Parser<T> =>
 
     return nextState
   })
+
+/**
+ * Takes two numbers and parser and returns a parser, that tries too match the given parser the amount of times between the two given numbers
+ * It fails only when when there too little matches
+ * In case there is too many matches it just stops parsing
+ * @param lowerBound A minimal number of matches
+ * @param upperBound A maximal number of matches
+ * @param parser A parser to be repeatedly matched
+ */
+export const between = <T>(lowerBound: number, upperBound: number) => (parser: Parser<T>): Parser<T[]> => {
+  return Parser.from((state) => {
+    // In this combinator using mutable state is actually a good idea
+    const results = []
+    let currState: ValidState<T>
+    let nextState: ParserState<T> = state
+    let i = 0
+
+    while (true) {
+      currState = nextState
+      nextState = parser.apply(currState)
+      if (nextState.__type__ === 'ErrorState' || i > upperBound) {
+        break
+      } else {
+        results.push(nextState.result)
+        i++
+      }
+    }
+    if (lowerBound <= i) {
+      return ResultState.update(currState, results, 0)
+    }
+
+    return ErrorState(state, `Combinator between: Should have found from ${lowerBound} to ${upperBound} matches, but found ${i}`)
+  })
+}
