4c786cbe7ea8ab29824f3203001ad642bfc1b613
Sławomir Kusiński <skwinged@gmail.com>
2020-07-06 13:19:45 +0200

    Feature: Combinator peek: Combinator function peekSafe added along with JSDocs

--- src/combinators/peek.ts
+++ src/combinators/peek.ts
@@ -16,3 +16,21 @@ export const peek = <T>(parser: Parser<T>): Parser<T | undefined> =>
 
     return ResultState.update(state, nextState.result, 0)
   })
+
+/**
+ * Takes in a parser and returns a parser that tries to match the given parser, but doesn't consume the input text
+ * It never fails
+ * If the given parser succeeds the result is the same as its result
+ * Otherwise the result is undefined
+ * @param parser The parser to be matched without consuming input
+ */
+export const peekSafe = <T>(parser: Parser<T>): Parser<T | undefined> =>
+  Parser.from((state) => {
+    const nextState = parser.apply(state)
+
+    if (nextState.__type__ === 'ErrorState') {
+      return ResultState.update(state, undefined, 0)
+    }
+
+    return ResultState.update(state, nextState.result, 0)
+  })
