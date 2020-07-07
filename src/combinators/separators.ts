39e71fb57026772020038270d5fdcaf2f82dbc4e
Sławomir Kusiński <skwinged@gmail.com>
2020-07-07 21:13:51 +0200

    Feature: Combinator separators: Combinator function separatedByBetween added along with JSDocs; separatedBy reimplemented using separatedByBetween

--- src/combinators/separators.ts
+++ src/combinators/separators.ts
@@ -2,60 +2,70 @@ import { Parser } from '../parser'
 import { ParserState, ResultState, ErrorState } from '../state'
 
 /**
+ * Matches the second of the given parsers between lowerBound and upperBound times, separated by the first of the given parsers
+ * @param lowerBound A minimal number of matches
+ * @param upperBound A maximal number of matches
+ * @param separator parser to match separators
+ * @param trailing optional parameter indicating wheter there should be trailing separators, defaults to 'never'
+ * @param parser parser to match the results
+ */
+export const separatedByBetween = <S>(
+  lowerBound: number,
+  upperBound: number
+) => (
+  separator: Parser<S>,
+  trailing: 'always' | 'never' | 'optional' = 'never'
+) => <T>(parser: Parser<T>): Parser<T[]> =>
+  Parser.from((state) => {
+    const results = []
+    let currState: ParserState<S | T>
+    let nextValState: ParserState<T> = state
+    let nextSepState: ParserState<S> = state
+
+    while (true) {
+      currState = nextValState
+      nextValState = parser.apply(nextSepState)
+
+      if (
+        nextValState.__type__ === 'ErrorState' ||
+        results.length >= upperBound
+      ) {
+        if (trailing !== 'never') {
+          currState = nextSepState
+        }
+
+        break
+      }
+
+      currState = nextSepState
+      nextSepState = separator.apply(nextValState)
+
+      if (nextSepState.__type__ === 'ErrorState') {
+        if (trailing !== 'always') {
+          currState = nextValState
+          results.push(nextValState.result)
+        }
+
+        break
+      }
+
+      results.push(nextValState.result)
+    }
+
+    if (results.length >= lowerBound) {
+      return ResultState.update(currState, results, 0)
+    }
+
+    return ErrorState(
+      state,
+      `Combinator separatedByBetween: Should have found from ${lowerBound} to ${upperBound} matches, but found ${results.length}`
+    )
+  })
+
+/**
  * Matches the second of the given parsers multiple times, separated by the first of the given parsers
  * @param separator parser to match separators
  * @param trailing optional parameter indicating wheter there should be trailing separators, defaults to 'never'
  * @param parser parser to match the results
  */
-export const separatedBy = <T, S>(
-  separator: Parser<S>,
-  trailing: 'always' | 'never' | 'optional' = 'never'
-) => (parser: Parser<T>): Parser<T[]> => {
-  return Parser.from((state) => {
-    const results = []
-    let currState: ParserState<S>
-    let nextValState: ParserState<T>
-    let nextSepState: ParserState<S> = state
-
-    while (true) {
-      currState = nextSepState
-      nextValState = parser.apply(currState)
-      nextSepState = separator.apply(nextValState)
-
-      if (nextValState.__type__ === 'ErrorState') {
-        break
-      }
-
-      if (nextSepState.__type__ === 'ResultState' && trailing === 'never') {
-        currState = ErrorState(
-          nextValState,
-          'Combinator separatedBy: trailing separator found'
-        )
-        break
-      }
-
-      if (nextSepState.__type__ === 'ErrorState' && trailing === 'always') {
-        currState = ErrorState(
-          nextValState,
-          'Combinator separatedBy: trailing separator not found'
-        )
-        break
-      }
-
-      results.push(nextValState.result)
-
-      if (nextSepState.__type__ === 'ErrorState' && trailing === 'optional') {
-        break
-      }
-    }
-
-    if (
-      currState.__type__ === 'ResultState' ||
-      currState.__type__ === 'InitialState'
-    ) {
-      return ResultState.update(currState, results, 0)
-    }
-
-    return ErrorState(state, currState.msg)
-  })
-}
+export const separatedBy = separatedByBetween(0, Infinity)
