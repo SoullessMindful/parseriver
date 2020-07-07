4cdbe633f09842a38df88d11281879cd7f6c9749
Sławomir Kusiński <skwinged@gmail.com>
2020-07-07 13:45:26 +0200

    Feature: Parser regexp: Tests: fixing formating in existing test file

--- src/parsers/regexp.letter.spec.ts
+++ src/parsers/regexp.letter.spec.ts
@@ -9,8 +9,9 @@ describe('letter: Unit testing', () => {
     ['head', 'h'],
     ['tail', 't'],
     ['rDay1', 'r'],
-    ['ż23 i81', 'ż']
-  ]).test('if it properly parses a letter',
+    ['ż23 i81', 'ż'],
+  ]).test(
+    'if it properly parses a letter',
     (text: string, expected: string) => {
       const state = letter.run(text)
 
@@ -30,8 +31,9 @@ describe('letter: Unit testing', () => {
     ['\nhead'],
     ['\ttail'],
     ['-r  1 Day1'],
-    ['23ż i81']
-  ]).test('if it properly return an error when the input doesn\'t start with a letter',
+    ['23ż i81'],
+  ]).test(
+    "if it properly return an error when the input doesn't start with a letter",
     (text: string) => {
       const state = letter.run(text)
 
@@ -44,43 +46,45 @@ describe('letter: Unit testing', () => {
 describe('letter: Property testing', () => {
   test('if the result is the first character of a string in a positive case', () => {
     fc.assert(
-      fc.property(fc.string(), fc.anything(), fc.nat(),
+      fc.property(
+        fc.string(),
+        fc.anything(),
+        fc.nat(),
         (text, prevResult, prevIndex) => {
           const prevState: ValidState<any> = {
             __type__: 'ResultState',
             result: prevResult,
             index: prevIndex,
-            text
+            text,
           }
           const state = letter.apply(prevState)
 
-          return (
-            (state.__type__ === 'ResultState')
-              ? state.result === prevState.text.charAt(0) // in a positive case result is the first character of input
-              : true
-          )
+          return state.__type__ === 'ResultState'
+            ? state.result === prevState.text.charAt(0) // in a positive case result is the first character of input
+            : true
         }
       )
     )
   })
 
-  test('if the index increases by one in a positive case and doesn\'t change in a negative case', () => {
+  test("if the index increases by one in a positive case and doesn't change in a negative case", () => {
     fc.assert(
-      fc.property(fc.string(), fc.anything(), fc.nat(),
+      fc.property(
+        fc.string(),
+        fc.anything(),
+        fc.nat(),
         (text, prevResult, prevIndex) => {
           const prevState: ValidState<any> = {
             __type__: 'ResultState',
             result: prevResult,
             index: prevIndex,
-            text
+            text,
           }
           const state = letter.apply(prevState)
 
-          return (
-            state.__type__ === 'ResultState'
-              ? state.index === prevState.index + 1 // in a positive case increases by 1
-              : state.index === prevState.index // in a negative case doesn't change
-          )
+          return state.__type__ === 'ResultState'
+            ? state.index === prevState.index + 1 // in a positive case increases by 1
+            : state.index === prevState.index // in a negative case doesn't change
         }
       )
     )
@@ -88,13 +92,17 @@ describe('letter: Property testing', () => {
 
   test('if it will always fail with digits in front of input string', () => {
     fc.assert(
-      fc.property(fc.string(), fc.anything(), fc.nat(), fc.nat(),
+      fc.property(
+        fc.string(),
+        fc.anything(),
+        fc.nat(),
+        fc.nat(),
         (text, prevResult, prevIndex, digits) => {
           const prevState: ValidState<any> = {
             __type__: 'ResultState',
             result: prevResult,
             index: prevIndex,
-            text: digits.toString() + text
+            text: digits.toString() + text,
           }
           const state = letter.apply(prevState)
 
