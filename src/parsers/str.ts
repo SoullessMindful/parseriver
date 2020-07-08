05ef9c797839704a5ab98345da21cabef149b13a
Sławomir Kusiński <skwinged@gmail.com>
2020-07-08 13:26:22 +0200

    Develop: Parser str: Hotfix: safeguards against unexpected end of input; error msg reformated

--- src/parsers/str.ts
+++ src/parsers/str.ts
@@ -13,6 +13,10 @@ export const str = (pattern: string): Parser<string> =>
 
       const shift = pattern.length
 
+      if (text.length < shift) {
+        return ErrorState(state, 'Parser str: unexpected end of input')
+      }
+
       if (text.startsWith(pattern)) {
         return ResultState.update(
           state, // updated state
@@ -23,7 +27,10 @@ export const str = (pattern: string): Parser<string> =>
 
       return ErrorState(
         state,
-        `str: was looking for "${pattern}", found "${text.slice(0, shift)}"`
+        `Parser str: was looking for "${pattern}", found "${text.slice(
+          0,
+          shift
+        )}"`
       )
     }
   )
